2a14a7d4a71a8ce42669ba4a04a93f1dfa295b31
Stephen Hackett <stephen@512pixels.net>
2014-08-07 11:11:00 -0500

    Update yt.sh
    Updated shell script with less ancient things

--- yt.sh
+++ yt.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-# wrapper around youtube-dl, from TJ Luoma (http://luo.ma)
+# ytsnatch, a wrapper around youtube-dl, from TJ Luoma (http://luo.ma)
 
 FOUND=no
 
 # these are the mp4 formats that YouTube supports, starting with the highest resolution
-for FORMAT in 38 37 22 18 17
+for FORMAT in 84 83 82 38 37 22 18 17
 do
 
-    echo "$NAME: Trying $FORMAT ..."
+echo "$NAME: Trying $FORMAT ..."
 
     if [ "$FOUND" = "no" ]
     then
 
-        youtube-dl -f "$FORMAT" --no-overwrites \
-        -o "/[INSERT PATH HERE]/%(stitle)s-%(id)s.%(ext)s" "$@" && FOUND=yes # to use with burninator.scpt, this path has to be hard-coded.
+youtube-dl -f "$FORMAT" --restrict-filenames --no-overwrites \
+        -o "/Users/kevin/Downloads/%(title)s-%(id)s.%(ext)s" "$@" && FOUND=yes # to use with burninator.scpt, this path has to be hard-coded.
 
     fi
 done
